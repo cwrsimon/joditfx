b4f37d53e52a5200bca5fba245ebb12ad3b6107d
Christian Simon <csimon@wesim.de>
2019-03-14 22:18:06 +0100

    code cleanup

--- src/main/java/de/wesim/summernotefx/SummerNoteEditor.java
+++ src/main/java/de/wesim/summernotefx/SummerNoteEditor.java
@@ -6,6 +6,8 @@ import org.w3c.dom.Element;
 import org.w3c.dom.events.EventTarget;
 import org.w3c.dom.html.HTMLAnchorElement;
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.logging.Level;
 import javafx.application.HostServices;
 import javafx.beans.property.BooleanProperty;
 import javafx.beans.property.SimpleBooleanProperty;
@@ -15,7 +17,6 @@ import javafx.concurrent.Worker.State;
 import javafx.scene.layout.StackPane;
 import javafx.scene.web.WebErrorEvent;
 import javafx.scene.web.WebView;
-import netscape.javascript.JSObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -24,7 +25,7 @@ import org.slf4j.LoggerFactory;
 // TODO Konsolidieren
 // TODO Lokalisierung !!!
 public class SummerNoteEditor extends StackPane {
-    
+
     final WebView webview = new WebView();
 
     private final HostServices hostServices;
@@ -45,7 +46,7 @@ public class SummerNoteEditor extends StackPane {
 
     public void setHTMLContent(String content) {
         final String content_js = StringEscapeUtils.escapeEcmaScript(content);
-        getLogger().info("Setting content...: {}", content_js);
+        getLogger().debug("Setting content...: {}", content_js);
         webview.getEngine().executeScript("setEditorContent('" + content_js + "');");
         setContentUpdate(false);
     }
@@ -61,7 +62,9 @@ public class SummerNoteEditor extends StackPane {
     }
 
     public void openURL(String url) {
-        hostServices.showDocument(url);
+        if (url.length() > 0) {
+            hostServices.showDocument(url);
+        }
     }
 
     public final void setContentUpdate(boolean newValue) {
@@ -79,22 +82,48 @@ public class SummerNoteEditor extends StackPane {
 
     // TODO CSSProperties 
     public SummerNoteEditor(HostServices hostServices, String editorContent) {
+        this.hostServices = hostServices;
+
         WebConsoleListener.setDefaultListener((WebView wv, String msg, int i, String source) -> {
             getLogger().info("JS Console [{}, {}]: {}", source, i, msg);
         });
+                
         final SummerNoteEditor backReference = this;
+        webview.getEngine().getLoadWorker().stateProperty().addListener(
+                new SummernoteWhenLoadedListener(backReference, editorContent, null));
+        webview.getEngine().getLoadWorker().exceptionProperty().addListener((ObservableValue<? extends Throwable> ov, Throwable t, Throwable t1) -> {
+            getLogger().error("SummernoteEditorFX Webview exception, ov: {}", ov.getValue().getLocalizedMessage(), ov.getValue());
+            getLogger().error("SummernoteEditorFX Webview exception, t: {}", t.getLocalizedMessage(), t);
+            getLogger().error("SummernoteEditorFX Webview exception, t1: {}", t1.getLocalizedMessage(), t1);
+        });
 
         webview.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
         webview.getEngine().setOnError(e -> {
             WebErrorEvent event = (WebErrorEvent) e;
             getLogger().error("{}", event.getException());
         });
+
+        var htmlSource = prepareHtmlSource();
+        webview.getEngine().loadContent(htmlSource);
+        this.getChildren().add(webview);
+
+    }
+
+    public Logger getLogger() {
+        return LoggerFactory.getLogger(getClass());
+    }
+
+    WebView getWebView() {
+        return this.webview;
+    }
+
+    private String prepareHtmlSource() {
         final String jqueryURL = SummerNoteEditor.class.getResource("/jquery.slim.min.js").toExternalForm();
         final String summernoteLiteJSURL = SummerNoteEditor.class.getResource("/summernote-lite.js").toExternalForm();
         final String summernoteCSSURL = SummerNoteEditor.class.getResource("/summernote-lite.css").toExternalForm();
         final String markJSURL = SummerNoteEditor.class.getResource("/jquery.mark.min.js").toExternalForm();
         final String i18NURL = SummerNoteEditor.class.getResource("/lang/summernote-de-DE.min.js").toExternalForm();
-        String htmlSource = null;
+        String htmlSource = "ERROR";
         try {
             htmlSource = new String(SummerNoteEditor.class.getResource("/de/wesim/summernotefx/summernote.html").openStream().readAllBytes(), "UTF-8");
             htmlSource = htmlSource.replace("%SUMMERNOTE_FONT%", SummerNoteEditor.class.getResource("/font/summernote.woff").toExternalForm());
@@ -104,28 +133,10 @@ public class SummerNoteEditor extends StackPane {
             htmlSource = htmlSource.replace("%MARK_JS_URL%", markJSURL);
             htmlSource = htmlSource.replace("%I18N_URL%", i18NURL);
 
-            getLogger().info(htmlSource);
-        } catch (IOException ex) {
-            getLogger().error("Loading failed:", ex);
+            getLogger().debug(htmlSource);
+        } catch (RuntimeException | IOException ex) {
+            getLogger().error("Preparing HTML source failed: {}", ex.getLocalizedMessage(), ex);
         }
-        webview.getEngine().loadContent(htmlSource);
-        this.getChildren().add(webview);
-
-        webview.getEngine().getLoadWorker().stateProperty().addListener(
-                new SummernoteWhenLoadedListener(backReference, hostServices, editorContent, null));
-        webview.getEngine().getLoadWorker().exceptionProperty().addListener((ObservableValue<? extends Throwable> ov, Throwable t, Throwable t1) -> {
-            getLogger().error("SummernoteEditorFX Webview exception, ov: {}", ov.getValue().getLocalizedMessage(), ov.getValue());
-            getLogger().error("SummernoteEditorFX Webview exception, t: {}", t.getLocalizedMessage(), t);
-            getLogger().error("SummernoteEditorFX Webview exceptionm, t1: {}", t1.getLocalizedMessage(), t1);
-        });
-        this.hostServices = hostServices;
-    }
-
-    public Logger getLogger() {
-        return LoggerFactory.getLogger(getClass());
-    }
-    
-    WebView getWebView() {
-        return this.webview;
+        return htmlSource;
     }
 }
--- src/main/java/de/wesim/summernotefx/SummernoteWhenLoadedListener.java
+++ src/main/java/de/wesim/summernotefx/SummernoteWhenLoadedListener.java
@@ -8,21 +8,21 @@ import javafx.concurrent.Worker;
 import javafx.scene.web.WebView;
 import netscape.javascript.JSObject;
 import org.w3c.dom.Element;
+import org.w3c.dom.events.Event;
+import org.w3c.dom.events.EventListener;
 import org.w3c.dom.events.EventTarget;
 import org.w3c.dom.html.HTMLAnchorElement;
 
 public class SummernoteWhenLoadedListener implements ChangeListener<Worker.State> {
 
     private final SummerNoteEditor backReference;
-    private final HostServices hostServices;
     private final String editorContent;
-    private WebView webview;
+    private final WebView webview;
     private final Map<String, String> cssProperties;
 
-    public SummernoteWhenLoadedListener(SummerNoteEditor caller, HostServices hostServices,
+    public SummernoteWhenLoadedListener(SummerNoteEditor caller,
             String editorContent, Map<String, String> cssProperties) {
         this.backReference = caller;
-        this.hostServices = hostServices;
         this.editorContent = editorContent;
         this.cssProperties = cssProperties;
         this.webview = caller.getWebView();
@@ -42,70 +42,55 @@ public class SummernoteWhenLoadedListener implements ChangeListener<Worker.State
             }
         }
         this.backReference.setHtmlText(this.editorContent);
-        // TODO Konsolidieren
-        // zweiten Click-Listener implementieren
+        // unfortunately, we don't have any class selectors in JavaFx ...
         var nodeList = this.webview.getEngine().getDocument().getElementsByTagName("div");
-        Element foundElement = null;
         for (int i = 0; i < nodeList.getLength(); i++) {
-            Element curElem = (Element) nodeList.item(i);
-            String classValue = curElem.getAttribute("class");
+            final Element curElem = (Element) nodeList.item(i);
+            final String classValue = curElem.getAttribute("class");
             if (classValue == null) {
                 continue;
             }
-            if (classValue.contains("note-editable")) { // note-link-popover
-                foundElement = curElem;
-                break;
+            if (classValue.contains("note-editable")) {
+                // clicks here are not supposed to open any urls
+                var eventTarget = (EventTarget) curElem;
+                eventTarget.addEventListener("click", new HandleAnchorClicksEventListener(false), false);
+                continue;
+            }
+            if (classValue.contains("note-link-popover")) {
+                // only this only may actually open a url
+                var eventTarget = (EventTarget) curElem;
+                eventTarget.addEventListener("click", new HandleAnchorClicksEventListener(true), false);
             }
         }
 
-        final EventTarget eventTarget = (EventTarget) foundElement;
-        eventTarget.addEventListener("click", evt -> {
+    }
 
-            final EventTarget target = evt.getTarget();
+    private class HandleAnchorClicksEventListener implements EventListener {
+
+        private final boolean openURL;
+
+        public HandleAnchorClicksEventListener(boolean openURL) {
+            this.openURL = openURL;
+        }
+
+        @Override
+        public void handleEvent(Event event) {
+            final EventTarget target = event.getTarget();
             if (!(target instanceof HTMLAnchorElement)) {
                 return;
             }
 
             final HTMLAnchorElement anchorElement = (HTMLAnchorElement) target;
-            final String url = anchorElement.getHref();
             final String targetAttr = anchorElement.getTarget();
             if (targetAttr.equals("_blank")) {
-                evt.preventDefault();
+                event.preventDefault();
             }
-        }, false);
-
-        nodeList = this.webview.getEngine().getDocument().getElementsByTagName("div");
-        foundElement = null;
-        for (int i = 0; i < nodeList.getLength(); i++) {
-            Element curElem = (Element) nodeList.item(i);
-            String classValue = curElem.getAttribute("class");
-            if (classValue == null) {
-                continue;
-            }
-            if (classValue.contains("note-link-popover")) { // note-link-popover
-                foundElement = curElem;
-                break;
-            }
-        }
-
-        var eventTarget2 = (EventTarget) foundElement;
-        eventTarget2.addEventListener("click", evt -> {
-
-            final EventTarget target = evt.getTarget();
-            if (!(target instanceof HTMLAnchorElement)) {
+            if (!openURL) {
                 return;
-            }
-
-            final HTMLAnchorElement anchorElement = (HTMLAnchorElement) target;
-            final String url = anchorElement.getHref();
-            final String targetAttr = anchorElement.getTarget();
-            if (targetAttr.equals("_blank")) {
-                evt.preventDefault();
             }
             // open url with host system's default
-            if (url.length() > 0 && hostServices != null) {
-                hostServices.showDocument(url);
-            }
-        }, false);
+            final String url = anchorElement.getHref();
+            backReference.openURL(url);
+        }
     }
 }
